c0b1bb132c86acfcab3bb27aaac109e8841ef857
hugotallys <htmo@ic.ufal.br>
2021-05-09 01:38:20 -0300

    refatorando codigo

--- myftp.py
+++ myftp.py
@@ -1,27 +1,57 @@
 import socket
 
+
+class FTPClient:
+
+    # Creates the client socket specifying the family (AF_INET ipv4)
+    # and making explicit that it is a TCP socket (SOCK_STREAM).
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def __init__(self, server_port, server_name):
+
+        self.server_port = server_port
+        self.server_name = server_name
+
+    def connect(self):
+        """
+        Connects to the server (3-way presentation).
+        """
+        self.client_socket.connect((self.server_name, self.server_port))
+
+    def send_to_server(self, bytes):
+        """
+        Sends bytes via client socket and to the TCP connection.
+        """
+        self.client_socket.send(bytes)
+
+    def recieve_from_server(self):
+        """
+        Recieves bytes from the server and through the TCP connection.
+        """
+        return self.client_socket.recv(2048)#what this number means ?
+
+    def terminate(self):
+        """
+        Terminates the session with the server.
+        """
+        self.client_socket.close()
+
+
 if __name__ == "__main__":
 
     server_name = "localhost"
     server_port = 12000
 
-    # cria o socket cliente especificando a família (AF_INET ipv4)
-    # e explicitando que é um socket TCP (SOCK_STREAM)
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # conecta ao servidor (apresentação de 3 vias)
-    client_socket.connect((server_name, server_port))
+    ftp_client = FTPClient(server_port=server_port, server_name=server_name)
+    ftp_client.connect()
 
     sentence = input("Input lowercase sentence:")
 
-    # envia a cadeia sentence pelo socket do cliente e para a conexao TCP
-    client_socket.send(bytes(sentence, "utf-8"))
+    ftp_client.send_to_server(bytes=bytes(sentence, "utf-8"))
 
-    # recebe a cadeia enviada pelo servidor
-    modified_sentence = client_socket.recv(2048)
+    modified_sentence = ftp_client.recieve_from_server()
 
-    # exibe a mensagem na tela
-    print("From server:", modified_sentence.decode("utf-8"))
+    # Outputs message on screen.
+    print("Recieved from server:", modified_sentence.decode("utf-8"))
 
-    # Encerra a conexão com o servidor
-    client_socket.close()
+    ftp_client.terminate()
--- myftpserver.py
+++ myftpserver.py
@@ -1,27 +1,50 @@
 import socket
 
+import netifaces
+
+
+class FTPServer:
+
+    def __init__(self, port_number):
+
+        self.port_number = port_number
+        self.ip_address = self.default_route_ip()
+
+    def run(self):
+        # Creates TCP Socket.
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Binds the port number to its socket.
+        server_socket.bind(("", self.port_number))
+
+        # Listens until a client "hits" the door.
+        server_socket.listen(1)  # Maximum of 1 connection in the queue (review this argument)
+
+        print(f"Server is running on {self.ip_address}:{self.port_number}")
+
+        while True:
+            # Creates the client connection socket
+            connection_socket, addr = server_socket.accept()
+
+            # Recieves the message sent by the client.
+            sentence = connection_socket.recv(2048)
+            captilized_sentence = sentence.upper()
+
+            # Sends the message back to the client, but captilzed.
+            connection_socket.send(captilized_sentence)
+            connection_socket.close()
+
+    def default_route_ip(self):
+        """
+        Returns the first ip address on the interface
+        associated with the default route.
+        """
+        # Name of the default interface.
+        iface = netifaces.gateways()["default"][netifaces.AF_INET][1]
+        return netifaces.ifaddresses(iface)[netifaces.AF_INET][0]["addr"]
+
+
 if __name__ == "__main__":
-    server_port = 12000
 
-    # cria o socket TCP
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # associa o numero da porta do servidor ao seu socket
-    server_socket.bind(("", server_port))
-
-    # server_socket é o socket de entrada
-    # ficamos "escutando" até que algum cliente bata
-
-    server_socket.listen(1)  # no máximo 1 conexão em fila
-
-    print("Server is listening")
-
-    while 1:
-        # cria o scoket de conexão dedicado ao cliente
-        connection_socket, addr = server_socket.accept()
-        # recebe a mensagem enviada pelo cliente
-        sentence = connection_socket.recv(2048)
-        captilized_sentence = sentence.upper()
-        # manda a mensagem de volta para o cliente
-        connection_socket.send(captilized_sentence)
-        connection_socket.close()
+    ftp_server = FTPServer(port_number=12000)
+    ftp_server.run()
