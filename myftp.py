3aee238d37c7b252e923c372cfc6a2cb1c3f76c6
hugotallys <htmo@ic.ufal.br>
2021-05-09 02:40:47 -0300

    implementa protocolo de comunicaçao basico

--- myftp.py
+++ myftp.py
@@ -3,19 +3,19 @@ import socket
 
 class FTPClient:
 
-    # Creates the client socket specifying the family (AF_INET ipv4)
-    # and making explicit that it is a TCP socket (SOCK_STREAM).
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     def __init__(self, server_port, server_name):
 
         self.server_port = server_port
         self.server_name = server_name
+        self.client_socket = None
 
     def connect(self):
         """
         Connects to the server (3-way presentation).
         """
+        # Creates the client socket specifying the family (AF_INET ipv4)
+        # and making explicit that it is a TCP socket (SOCK_STREAM).
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((self.server_name, self.server_port))
 
     def send_to_server(self, bytes):
@@ -36,6 +36,20 @@ class FTPClient:
         """
         self.client_socket.close()
 
+    def parse_command(self, command):
+        """
+        Breaks the command in two components: the command itself
+        and parameter (if any).
+
+        :param command: The input string representing the command.
+        """
+        parsed = command.strip(" ").split(" ")
+
+        if len(parsed) == 1:
+            return parsed[0], ""
+        else:
+            return parsed[0], parsed[1]
+
 
 if __name__ == "__main__":
 
@@ -43,15 +57,24 @@ if __name__ == "__main__":
     server_port = 12000
 
     ftp_client = FTPClient(server_port=server_port, server_name=server_name)
-    ftp_client.connect()
 
-    sentence = input("Input lowercase sentence:")
+    command = input("myftp>")
+    cmd, param = ftp_client.parse_command(command=command)
 
-    ftp_client.send_to_server(bytes=bytes(sentence, "utf-8"))
+    while cmd != "quit":
 
-    modified_sentence = ftp_client.recieve_from_server()
+        ftp_client.connect()
 
-    # Outputs message on screen.
-    print("Recieved from server:", modified_sentence.decode("utf-8"))
+        ftp_client.send_to_server(bytes=bytes(f"{cmd}#{param}\n", "utf-8"))
 
-    ftp_client.terminate()
+        modified_sentence = ftp_client.recieve_from_server()
+
+        # Outputs message on screen.
+        print("Recieved from server:", modified_sentence.decode("utf-8"))
+
+        command = input("myftp>")
+        cmd, param = ftp_client.parse_command(command=command)
+
+        ftp_client.terminate()
+
+    print("Bye!")
