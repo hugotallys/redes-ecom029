50f774eda0086956a10c3040629d9a72529d7691
hugotallys <htmo@ic.ufal.br>
2021-05-16 00:04:33 -0300

    adiciona constantes ao modulo

--- utils.py
+++ utils.py
@@ -3,6 +3,13 @@ import netifaces
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096  # Recieves 4096 bytes each time step.
 
+ERROR = "0"
+SUCCESS = "1"
+
+PROMPT_MESSAGE = "PomptMessage"
+LIST_MESSAGE = "ListMessage"
+FILE_MESSAGE = "FileMessage"
+
 
 def default_route_ip():
     """
